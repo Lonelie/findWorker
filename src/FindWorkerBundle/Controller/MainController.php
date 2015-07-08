b15e9ea6649ef05f088f875d9a0e4ba1fc5ffbc0
Goundo Djikine <goundo-djikine@cifacom.com>
2015-07-08 16:05:54 +0200

    add homepage controller and routes

---
+++ src/FindWorkerBundle/Controller/MainController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace FindWorkerBundle\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+/**
+ * Class MainController
+ * @package FindWorkerBundle\Controller
+ */
+class MainController extends Controller
+{
+    /**
+     * Page d'accueil
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function homepageAction()
+    {
+        return $this->render('FindWorkerBundle:Main:homepage.html.twig');
+    }
+}
